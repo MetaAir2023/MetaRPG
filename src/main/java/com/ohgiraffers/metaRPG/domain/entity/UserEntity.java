00dbf32cc3a91425ec1b733963c3d97e10e68906
Soyeon Park <aqua343@gmail.com>
2023-06-15 14:18:08 +0900

    User DB, Entity, Repository 구현

--- src/main/java/com/ohgiraffers/metaRPG/domain/entity/UserEntity.java
+++ src/main/java/com/ohgiraffers/metaRPG/domain/entity/UserEntity.java
@@ -1,4 +1,18 @@
 package com.ohgiraffers.metaRPG.domain.entity;
 
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
 public class UserEntity {
+    private int sequence;
+    private String name;
+    private int hp;
+    private int level;
+    private int money;
+    private int itemSequence;
+    private int itemUpLv;
 }
--- src/main/java/com/ohgiraffers/metaRPG/infra/UserDB.java
+++ src/main/java/com/ohgiraffers/metaRPG/infra/UserDB.java
@@ -1,4 +1,35 @@
 package com.ohgiraffers.metaRPG.infra;
 
+import com.ohgiraffers.metaRPG.domain.entity.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Repository
 public class UserDB {
+
+    private final Map<Integer, UserEntity> userList;
+
+    public UserDB() {
+        this.userList = new HashMap<>();
+        this.userList.put(1, new UserEntity(1, "소드마스터", 100, 1, 5000, 1, 1));
+    }
+
+    public List<UserEntity> findAllUsers() {
+
+        return new ArrayList<>(userList.values());
+    }
+
+    public UserEntity findUserBySequence(int sequence) {
+
+        return userList.get(sequence);
+    }
+
+    public UserEntity findUserByName(String name) {
+
+        return userList.get(name);
+    }
 }

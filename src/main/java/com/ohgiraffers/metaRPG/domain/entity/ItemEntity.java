a51fe721c60286050669d7ed89395613ce1f23da
jeongjiwon <jeewonre@hufs.ac.kr>
2023-06-16 16:38:03 +0900

    [#38] UpgradeDomainService 수정

--- src/main/java/com/ohgiraffers/metaRPG/domain/entity/ItemEntity.java
+++ src/main/java/com/ohgiraffers/metaRPG/domain/entity/ItemEntity.java
@@ -1,15 +1,27 @@
 package com.ohgiraffers.metaRPG.domain.entity;
 
-import lombok.*;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@ToString
 public class ItemEntity {
     private int sequence;
     private String name;
-    private int attackScore;
     private int tier;
+
+    public ItemEntity(int sequence, String name, int tier) {
+        this.sequence = sequence;
+        this.name = name;
+        this.tier = tier;
+    }
+
+    public ItemEntity get() {
+        return this;
+    }
+
+    public Integer getTier() {
+        return this.tier;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
 }
--- src/main/java/com/ohgiraffers/metaRPG/infra/ItemDB.java
+++ src/main/java/com/ohgiraffers/metaRPG/infra/ItemDB.java
@@ -1,7 +1,6 @@
 package com.ohgiraffers.metaRPG.infra;
 
 import com.ohgiraffers.metaRPG.domain.entity.ItemEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -16,7 +15,10 @@ public class ItemDB {
 
     public ItemDB() {
         this.itemList = new HashMap<>();
-        this.itemList.put(1, new ItemEntity(1,"무한의 대검", 100, 1));
+        this.itemList.put(1, new ItemEntity(1,"무한의 대검", 3));
+        this.itemList.put(2, new ItemEntity(2,"BF 대검", 2));
+        this.itemList.put(3, new ItemEntity(3,"롱소드", 1));
+        this.itemList.put(4, new ItemEntity(4,"도란의 검", 1));
     }
 
     public List<ItemEntity> findAllItems() {
